06addf12e97e1a6a635b73bc36fc25e7b3b040fd
asb1512 <andrew@olympicmedia.com>
2022-08-18 02:07:26 -0400

    title disappears on other pages

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useSpring } from '@react-spring/core';
+import { animated } from '@react-spring/web';
 
 const str = 'Andrew Bourgeois';
 
-export default function Title() {
+export default function Title({ page }) {
   const [titleText, setTitleText] = useState('');
 
-  useEffect(() => {
-    
+  // this should be changed out for a mount/unmount transition
+  const titleSpring = useSpring({
+    opacity: page === 'home' ? 1 : 0,
   });
-
+  
   return (
-    <div className="title-cntr">
+    <animated.div className="title-cntr" style={titleSpring}>
       <h1 className="blinking-cursor">
         Andrew
         <br />
         Bourgeois
       </h1>
       <h3>full-stack developer</h3>
-    </div>
+    </animated.div>
   );
 }
