26a58491b5c8094f76f6758b351f8283db660d7d
asb1512 <andrew@olympicmedia.com>
2022-08-22 16:01:48 -0400

    building out about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function About({ page }) {
+export default function About() {
   return (
-    <div style={{ fontSize: '6rem'}}>Kiss me Cody You?</div>
+    <div className="about-cntr">
+      <h1>
+        about
+      </h1>
+      <p>
+        Hi I'm Andrew. I love creating web experiences that are beautiful,
+        functional and reliable.
+      </p>
+    </div>
   );
 }
