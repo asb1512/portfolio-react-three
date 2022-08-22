369bd04f0b4e780ce185c1b278c14e117837195f
asb1512 <andrew@olympicmedia.com>
2022-08-22 16:06:51 -0400

    passing current path as prop; need a 3D element for the main camera to look to

--- src/3d/HomeMarker.jsx
+++ src/3d/HomeMarker.jsx
@@ -2,19 +2,19 @@ import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
-export default function AboutMarker({ pathname }) {
+export default function HomeMarker({ pathname }) {
   const markerRef = useRef();
   const vec = new THREE.Vector3();
 
   useFrame((state) => {
-    if (pathname === '/about') {
-      state.camera.position.lerp(vec.set(0, 0.7, 0.45), 0.15);
+    if (pathname === '/') {
+      state.camera.position.lerp(vec.set(0, 0, 0), 0.15);
       state.camera.updateProjectionMatrix();
     }
   });
 
   return (
-    <mesh ref={markerRef} position={[1, 3, 1]}>
+    <mesh ref={markerRef}>
       <coneBufferGeometry attach="geometry" args={[0.1, 0.1, 4]} />
       <meshLambertMaterial attach="material" />
     </mesh>
