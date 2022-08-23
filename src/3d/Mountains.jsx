ffb1c9d569a0f1cb1789408364d39acca8c8b95b
asb1512 <andrew@olympicmedia.com>
2022-08-23 01:13:47 -0400

    changing mesh color based on current path

--- src/3d/Mountains.jsx
+++ src/3d/Mountains.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
 export default function Model({ pathname }) {
-  const { nodes, materials } = useGLTF('/ridges_valleys.gltf');
+  const [color, setColor] = useState('#FFF');
+  const { nodes } = useGLTF('/ridges_valleys.gltf');
   const meshRef = useRef();
 
   useFrame(({ clock }) => {
@@ -12,13 +13,33 @@ export default function Model({ pathname }) {
     }
   });
 
+  // set mesh color based on current 'page'
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        setColor('#FFF');
+        break;
+      case '/about':
+        setColor('red');
+        break;
+      default: 
+        setColor('#FFF');
+        break;
+    }
+  }, [pathname]);
+
   return (
-    <group ref={meshRef} dispose={null} position={[0, -1.5, 0]}>
+    <group
+      ref={meshRef}
+      dispose={null}
+      position={[0, -1.5, 0]}
+    >
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Landscape002.geometry}
         material={nodes.Landscape002.material}
+        material-color={color}
         scale={[4.28, 4.07, 4.28]}
       />
     </group>
