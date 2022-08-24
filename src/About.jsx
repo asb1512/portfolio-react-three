80e137fb1c50b4ab9f15e1d99ead5c0e9a1c4fe5
asb1512 <andrew@olympicmedia.com>
2022-08-24 11:14:55 -0400

    added image to about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import napTime from './images/nap-time.jpg';
 
 export default function About() {
   return (
-    <div className="about-cntr">
+    <div className="page-cntr">
       <h1>
         about
       </h1>
@@ -10,6 +11,15 @@ export default function About() {
         Hi I'm Andrew. I love creating web experiences that are beautiful,
         functional and reliable.
       </p>
+      <p>
+        I primarily use React.js to design the frontend and UI, along with RTK
+        query for communication with an Express.js backend and database. 
+      </p>
+      <img
+        src={napTime}
+        alt="Andrew Bourgeois: Full-Stack Developer"
+        className="about-img"
+      />
     </div>
   );
 }
