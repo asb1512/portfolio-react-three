7f96329b1a5d8d256c1e33c04348582f6c56cb1c
asb1512 <andrew@olympicmedia.com>
2022-09-08 00:18:18 -0400

    building out work items

--- src/Work.jsx
+++ src/Work.jsx
@@ -4,6 +4,23 @@ export default function Work() {
   return (
     <div className="page-cntr">
       <h1>Work</h1>
+      <div className="work-content">
+        <div className="work-item">
+          CNCT
+        </div>
+        <div className="work-item">
+          Dynamic Pitch
+        </div>
+        <div className="work-item">
+          We The People Wine
+        </div>
+        <div className="work-item">
+          AdSize (Flatiron)
+        </div>
+        <div className="work-item">
+          ParkList (Flatiron)
+        </div>
+      </div>
     </div>
   );
 }
