e7223eb605ea35d2289cee7064b001aa6ce07813
asb1512 <andrew@olympicmedia.com>
2025-03-23 23:24:44 -0400

    remove unnecessary content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Scene from './3d/Scene';
-import Nav from './Nav';
-import Pages from './Pages';
-import Accents from './Accents';
 import './App.css';
 
 function App() {
@@ -11,11 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Nav pathname={location.pathname} />
-      <Pages pathname={location.pathname} />
-      <div className="blur-cover" pathname={location.pathname} />
       <Scene pathname={location.pathname} />
-      <Accents pathname={location.pathname} />
     </div>
   );
 }
